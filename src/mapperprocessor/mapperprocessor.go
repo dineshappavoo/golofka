538422c4f26ad67fc856b02b3edfc5573d0fbf46
dinesgappavoo <dinesha.cit@gmail.com>
2015-04-02 05:51:40 -0500

    Initial commit

---
+++ src/mapperprocessor/mapperprocessor.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	//"log"
+)
+
+type Tweet struct {
+	User     string
+	Message  string
+	Retweets int
+}
+
+//
+func mapper(object string) (interface{}, error) {
+
+	fmt.Println("Testing mapper")
+	tweet := Tweet{User: "olivere", Message: "Take Five", Retweets: 0}
+	return tweet, nil
+}
+
+func processor(object interface{}) error {
+	fmt.Println(object.(Tweet).User)
+	fmt.Println(object.(Tweet).Message)
+	fmt.Println(object.(Tweet).Retweets)
+	return nil
+}
+
+func main() {
+	content, _ := mapper("rereterte  erter ")
+	processor(content)
+}
---
+++ src/readlog.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	//"log"
+	"os"
+)
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+	path := "/Users/Dany/Documents/FALL-2013-COURSES/Imp_Data_structures/workspace/golofka/src/vmlog.log"
+	lines, _ := readLines(path)
+	for i, _ := range lines {
+		fmt.Println("Line Object ",i)
+		fmt.Println(lines[i])
+	}
+}
